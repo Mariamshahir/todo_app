2dce7689f3a3489f27550c7e37b266ccdbe3068d
Mariam Shahir <mariamshahir2000@gmail.com>
2024-02-28 03:48:21 +0200

    Finished list tap.

--- lib/taps/list_tap.dart
+++ lib/taps/list_tap.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:todo/widget/task_widget.dart';
 
 class ListTap extends StatelessWidget {
   const ListTap({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+                Expanded(
+                  child: ListView.builder(
+                      itemCount:10,
+                      itemBuilder: (context,index){
+                        return TaskWidget();
+                      }),
+                )
+      ],
+    );
   }
 }
