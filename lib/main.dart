06bbe2a7bff14f71700da0e7b2180a65cd904a9c
Mariam Shahir <mariamshahir2000@gmail.com>
2024-02-28 03:48:01 +0200

    Finished main screen. Add language provider , theme provider and shared preferences.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/homescreen.dart';
 import 'package:todo/provider/language_provider.dart';
 import 'package:todo/provider/theme_provider.dart';
 import 'package:todo/splash.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:todo/utils/aap_theme.dart';
-import 'package:provider/provider.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,42 +16,43 @@ void main() async{
   ThemeProvider themeProvider =ThemeProvider();
   await themeProvider.setItems();
   runApp(MultiProvider(
-      providers:[
+      providers: [
         ChangeNotifierProvider(create: (_) => languageProvider),
         ChangeNotifierProvider(create: (_) => themeProvider),
       ],
-      child: const MyApp()
-  ));
+      child: const MyApp()),
+  );
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     LanguageProvider provider=Provider.of(context);
     ThemeProvider themeProvider=Provider.of(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      localizationsDelegates: const [
+      localizationsDelegates: const[
+        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      supportedLocales: const [
+      supportedLocales: const[
         Locale("en"),
         Locale("ar")
       ],
+      locale: Locale(provider.currentLocale),
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
       themeMode: themeProvider.currentTheme,
       routes: {
         Splash.routeName:(_) => Splash(),
-        HomeScreen.routeName: (_) => HomeScreen(),
+        HomeScreen.routeName:(_) => HomeScreen(),
       },
       initialRoute: Splash.routeName,
     );
   }
 }
+
+
 
