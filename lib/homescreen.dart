95e199f57900f5a43292edb882ee10267faf2753
Mariam Shahir <mariamshahir2000@gmail.com>
2024-03-11 12:05:41 +0200

    Finished logout.

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo/models/myuser.dart';
+import 'package:todo/provider/list_provider.dart';
 import 'package:todo/provider/theme_provider.dart';
 import 'package:todo/tabs/list_tab.dart';
 import 'package:todo/tabs/settings_tab.dart';
+import 'package:todo/user/login.dart';
 import 'package:todo/utils/app_colors.dart';
 import 'package:todo/bottoms/add_bottom.dart';
+import 'package:todo/utils/app_language.dart';
 
 class HomeScreen extends StatefulWidget {
   static const String routeName = "homescreen";
+
   const HomeScreen({super.key});
 
   @override
@@ -16,20 +20,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  List<Widget> taps = [
-    const ListTab(),
-    const Settings()
-  ];
+  List<Widget> taps = [const ListTab(), const Settings()];
   late ThemeProvider themeProvider;
-  int currentIndexTab=0;
+  late ListProvider listProvider;
+  int currentIndexTab = 0;
 
   @override
   Widget build(BuildContext context) {
-    themeProvider=Provider.of<ThemeProvider>(context);
+    themeProvider = Provider.of<ThemeProvider>(context);
+    listProvider = Provider.of(context);
     return Container(
-      decoration: BoxDecoration(
-        color: themeProvider.background
-      ),
+      decoration: BoxDecoration(color: themeProvider.background),
       child: Scaffold(
         backgroundColor: AppColors.transparent,
         appBar: buildAppBar(),
@@ -42,16 +43,29 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   AppBar buildAppBar() => AppBar(
-    title: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text("Welcome ${Myuser.currentUser!.userName}"),
-      ],
-    ),
-  );
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+                "${context.getLocalizations.welcome} ${Myuser.currentUser!.userName}"),
+          ],
+        ),
+        actions: [
+          InkWell(
+              onTap: () {
+                listProvider.clearData();
+                Navigator.pushReplacementNamed(context, Login.routeName);
+              },
+              child: Icon(
+                Icons.logout,
+                color: themeProvider.logout,
+              ))
+        ],
+      );
 
-  Widget buildBottomNavigationBar(context) =>Theme(
-      data: Theme.of(context).copyWith(canvasColor: Theme.of(context).primaryColor),
+  Widget buildBottomNavigationBar(context) => Theme(
+        data: Theme.of(context)
+            .copyWith(canvasColor: Theme.of(context).primaryColor),
         child: BottomAppBar(
           color: themeProvider.buttonAppBar,
           shape: const CircularNotchedRectangle(),
@@ -60,27 +74,40 @@ class _HomeScreenState extends State<HomeScreen> {
           clipBehavior: Clip.hardEdge,
           child: SingleChildScrollView(
             child: BottomNavigationBar(
-                currentIndex: currentIndexTab,
-                onTap: (newTabIndex){
-            currentIndexTab = newTabIndex;
-            setState(() {});
-                }, items: const[
-                BottomNavigationBarItem(icon: Icon(Icons.list_outlined,),label: "List"),
-                BottomNavigationBarItem(icon: Icon(Icons.settings),label: "Setting")
-              ],),
+              currentIndex: currentIndexTab,
+              onTap: (newTabIndex) {
+                currentIndexTab = newTabIndex;
+                setState(() {});
+              },
+              items: const [
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.list_outlined,
+                    ),
+                    label: "List"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.settings), label: "Setting")
+              ],
+            ),
           ),
         ),
       );
 
   FloatingActionButton buildFloatingActionButton() => FloatingActionButton(
-    onPressed: (){
-      showModalBottomSheet(context: context, isScrollControlled: true,builder: (context){
-        return Padding(
-          padding: MediaQuery.of(context).viewInsets,
-          child: const AddBottom(),
-        );
-      });
-    },child: const Icon(Icons.add,color: AppColors.white,),
-  );
-  }
-
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (context) {
+                return Padding(
+                  padding: MediaQuery.of(context).viewInsets,
+                  child: const AddBottom(),
+                );
+              });
+        },
+        child: const Icon(
+          Icons.add,
+          color: AppColors.white,
+        ),
+      );
+}
--- lib/provider/list_provider.dart
+++ lib/provider/list_provider.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:todo/models/myuser.dart';
 import 'package:todo/models/todo_model.dart';
 
 class ListProvider extends ChangeNotifier {
@@ -13,21 +14,31 @@ class ListProvider extends ChangeNotifier {
 
   Future<void> refreshTodo() async {
     todos.clear();
-    CollectionReference todoCollection =
-    FirebaseFirestore.instance.collection(Todo.collectionName);
-    QuerySnapshot querySnapshot =
-    await todoCollection.get();
-    todos = querySnapshot.docs.map((doc) {
-      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
-      Timestamp dateAsTime = data["date"];
-      return Todo(
-        id: data["id"],
-        task: data["title"],
-        description: data["description"],
-        dateTime: dateAsTime.toDate(),
-        isDone: data["isDone"],
-      );
-    }).toList();
+    Myuser? currentUser = Myuser.currentUser;
+    if (currentUser != null) {
+      CollectionReference todoCollection = FirebaseFirestore.instance
+          .collection(Myuser.collectionName)
+          .doc(currentUser.id)
+          .collection(Todo.collectionName);
+      QuerySnapshot querySnapshot = await todoCollection.orderBy("date").get();
+      todos = querySnapshot.docs.map((doc) {
+        Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+        Timestamp dateAsTime = data["date"];
+        return Todo(
+          id: data["id"],
+          task: data["title"],
+          description: data["description"],
+          dateTime: dateAsTime.toDate(),
+          isDone: data["isDone"],
+        );
+      }).toList();
+    }
     notifyListeners();
+  }
+
+  void clearData() {
+    todos = [];
+    selectedDate = DateTime.now();
+    Myuser.currentUser = null;
   }
 }
