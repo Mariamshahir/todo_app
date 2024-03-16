cc2966dace621cf54c8f97f00fa12e86dca1ef6d
Mariam Shahir <mariamshahir2000@gmail.com>
2024-03-16 20:01:59 +0200

    Finished add task in Firebase.

--- lib/models/todo_model.dart
+++ lib/models/todo_model.dart
@@ -1,15 +1,10 @@
 class Todo {
-  static const String collectionName ="todo";
+  static const String collectionName = "todo";
   String? id;
   String? task;
   String? description;
   DateTime? dateTime;
   bool? isDone;
 
-  Todo({ this.id,
-     this.task,
-     this.description,
-     this.dateTime,
-     this.isDone});
-
+  Todo({this.id, this.task, this.description, this.dateTime, this.isDone});
 }
